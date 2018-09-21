65068fd231e3bb98f2815a4005e45e663069ecff
Raden Adhyaksa <raksa69@gmail.com>
2018-09-21 20:09:56 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Raleway', sans-serif;
+                font-family: 'Raleway', sans-serif ;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
@@ -60,7 +60,7 @@
             }
 
             .m-b-md {
-                margin-bottom: 30px;
+                margin-bottom: 30px; 
             }
         </style>
     </head>
