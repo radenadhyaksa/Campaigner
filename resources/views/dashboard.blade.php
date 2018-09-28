a483f6ba4ad3399a46843a2e66300c2fd29cb6e4
Raden Adhyaksa <raksa69@gmail.com>
2018-09-28 15:51:45 +0700

    edit dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,7 +26,7 @@
                                     <td>
                                         {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
                                             {{Form::hidden('_method', 'DELETE')}}
-                                            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                            {{Form::submit('Delete', ['class' => 'btn btn-default'])}}
                                         {!!Form::close()!!}
                                     </td>
                                 </tr>
